89185b001b738d9e7371a0fde75ce9f6c193f99b
GB <gonzbosco@icloud.com>
2026-04-17 14:46:52 +0200

    aeroport modification
    Added a method to obtain airport information in a formatted string and removed unused methods.

--- src/main/java/fr/isep/projetassite/Aeroport.java
+++ src/main/java/fr/isep/projetassite/Aeroport.java
@@ -1,31 +1,21 @@
+package fr.isep.projetassite;
+
 public class Aeroport {
-    
     private String nom;
     private String ville;
     private String codeIATA;
 
-    
     public Aeroport(String nom, String ville, String codeIATA) {
         this.nom = nom;
         this.ville = ville;
         this.codeIATA = codeIATA;
     }
 
-    public String getNom() { return nom; }
-    public void setNom(String nom) { this.nom = nom; }
-
-    public String getVille() { return ville; }
-    public void setVille(String ville) { this.ville = ville; }
+    public String obtenirInfos() {
+        return "Aéroport: " + nom + " (" + codeIATA + ") - Ville: " + ville;
+    }
 
     public String getCodeIATA() { return codeIATA; }
-    public void setCodeIATA(String codeIATA) { this.codeIATA = codeIATA; }
-
-    @Override
-    public String toString() {
-        return "Aeroport{" +
-                "nom='" + nom + '\'' +
-                ", ville='" + ville + '\'' +
-                ", codeIATA='" + codeIATA + '\'' +
-                '}';
-    }
+    public String getNom() { return nom; }
+    public String getVille() { return ville; }
 }
